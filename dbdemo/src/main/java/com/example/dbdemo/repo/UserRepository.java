34a4e6bcdeba4f2bde2d15b627cda8f686130929
DhakshinaMoorthy, Venkateshkum <Venkateshkum.DhakshinaMoorthy@merrillcorp.com>
2019-06-28 16:36:36 +0530

    test

---
+++ dbdemo/src/main/java/com/example/dbdemo/repo/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.dbdemo.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.dbdemo.model.*;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+
+
+}
